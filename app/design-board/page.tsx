ca3a6da8dc3377fc824812c07cae21330234821a
Sampath_Kumara <84168369+sampath-kumaramd@users.noreply.github.com>
2024-03-08 03:06:48 +0530

    Initial commit

---
+++ app/design-board/page.tsx
@@ -0,0 +1,13 @@
+import { UserButton } from "@clerk/nextjs";
+import React from "react";
+
+type Props = {};
+
+export default function page({}: Props) {
+  return (
+    <div>
+      pages
+      <UserButton />
+    </div>
+  );
+}
