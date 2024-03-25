8d4fc8973307a584145cae95fb73e107fe345c13
Sampath_Kumara <84168369+sampath-kumaramd@users.noreply.github.com>
2024-03-26 04:08:08 +0530

    Config basic DND

--- app/(pages)/design-board/page.tsx
+++ app/(pages)/design-board/page.tsx
@@ -1,84 +1,9 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import DndExample from "@/components/DndExample";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <div className={styles.topHeader}>Select your Metal Color</div>
-      <div className={styles.horizontalLine} />
-      <div className={styles.top}>
-        <div className={styles.rings}>
-          <Image
-            src="/Graphic_Metal_Ring_G.png"
-            alt="Ring 1"
-            width={40}
-            height={40}
-          />
-          <Image
-            src="/Graphic_Metal_Ring_AG.png"
-            alt="Ring 2"
-            width={40}
-            height={40}
-          />
-          <Image
-            src="/Graphic_Metal_Ring_S.png"
-            alt="Ring 3"
-            width={40}
-            height={40}
-          />
-          <Image
-            src="/Graphic_Metal_Ring_AS.png"
-            alt="Ring 4"
-            width={40}
-            height={40}
-          />
-          <Image
-            src="/Graphic_Metal_Ring_GM.png"
-            alt="Ring 5"
-            width={40}
-            height={40}
-          />
-        </div>
-      </div>
-      <div className={styles.bottom}>
-        <div className={styles.left}>
-        <div className={styles.rings}>
-          <Image
-            src="/Graphic_Metal_Ring_G.png"
-            alt="Ring 1"
-            width={40}
-            height={40}
-          />
-          <Image
-            src="/Graphic_Metal_Ring_AG.png"
-            alt="Ring 2"
-            width={40}
-            height={40}
-          />
-          <Image
-            src="/Graphic_Metal_Ring_S.png"
-            alt="Ring 3"
-            width={40}
-            height={40}
-          />
-          <Image
-            src="/Graphic_Metal_Ring_AS.png"
-            alt="Ring 4"
-            width={40}
-            height={40}
-          />
-          <Image
-            src="/Graphic_Metal_Ring_GM.png"
-            alt="Ring 5"
-            width={40}
-            height={40}
-          />
-        </div>
-          <div className={styles.leftContent}></div>
-        </div>
-        <div className={styles.center}></div>
-        <div className={styles.right}></div>
-      </div>
-    </div>
+    <DndExample />
   );
 }
--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: [
-          "res.cloudinary.com",
-          "placehold.co"
-        ],
-      },
+  images: {
+    domains: [
+      "res.cloudinary.com",
+      "placehold.co"
+    ],
+  },
+  reactStrictMode: false,
 };
 
 export default nextConfig;
