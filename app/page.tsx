ad410e91772e0e24cd592f00a2c004d5c7def0db
Sampath_Kumara <84168369+sampath-kumaramd@users.noreply.github.com>
2024-03-26 05:28:10 +0530

    updae env in deployement

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       <div className="  m-4">
         <span className=" text-red-600">Note -</span> This will be your website
         and i need to place bellow button to website. <br /> you can navigate to
-        design board with bellow button.<br/>
+        design board with bellow button .<br/>
         you need to add user name and passowrd to login to design board.
       </div>
       <div className="justify-center items-center flex align-middle h-[88vh]">
