5ceecd9cb66d5640f2f6abb420fb55186d486a36
Sampath_Kumara <84168369+sampath-kumaramd@users.noreply.github.com>
2024-05-16 05:08:22 +0530

    add routes

--- next.config.mjs
+++ next.config.mjs
@@ -10,7 +10,16 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: '/about',
+        source: '/',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 's-maxage=1, stale-while-revalidate=59',
+          },
+        ],
+      },
+      {
+        source: '/design-board',
         headers: [
           {
             key: 'Cache-Control',
